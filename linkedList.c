a410cd85c1dba54ba1c21c31b3297328fc5dabdd
yujeong <kreamno@gmail.com>
2018-12-06 15:41:04 +0900

    04

--- linkedList.c
+++ linkedList.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef struct linknd {				//밑에서 자주 쓸 거면 typedef 사용하는 게 좋음. 
 	int data;
 	
@@ -5,5 +8,25 @@ typedef struct linknd {				//밑에서 자주 쓸 거면 typedef 사용하는 게 좋음.
 	struct linknd *next;
 } linknd_t;				
 
-static linknd_t *list;		//linknd list 실체, static 넣으면 정적 할당: 밖에서 접근할 수 없음. 
+static linknd_t *list;		//linknd list 실체, static 넣으면 정적 할당: 밖에서 접근할 수 없음.
+
+linknd_t* create_node(int value){
+	linknd_t *ndPtr;
+	
+	//동적 메모리 할당
+	ndPtr = (linknd_t*)malloc(sizeof(linknd_t));
+	if (ndPtr == NULL){
+		
+		printf("!!!ERROR!!!\n");
+		return NULL;
+	}
+	
+	//정수값 저장 
+	ndPtr->data = value;
+	ndPtr->next = NULL;
+	
+
+	return ndPtr; 
+
+} 
 
