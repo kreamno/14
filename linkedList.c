f24412ca9f634c3f53a77d39e4953bc94f0dacbb
yujeong <kreamno@gmail.com>
2018-12-06 15:23:27 +0900

    02

--- linkedList.c
+++ linkedList.c
@@ -1,3 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+typedef struct linknd {				//밑에서 자주 쓸 거면 typedef 사용하는 게 좋음. 
+	int data;
+	
+	//void *next; 가르키는 대상이 구조체임을 알고 있어서 void로 선언해도 괜찮다. 
+	struct linknd *next;
+} linknd_t;				
+
 
